ad54814d4e71f381a19219b4e227fc9f7022ffa1
Brad Reardon <brad.jay.reardon@gmail.com>
2014-01-05 04:00:18 -0500

    Finished basic functionality. Just gotta flesh out plugin.yml.

--- src/main/java/com/github/bradreardon/xpguardian/XPGuardian.java
+++ src/main/java/com/github/bradreardon/xpguardian/XPGuardian.java
@@ -1,6 +1,11 @@
 package com.github.bradreardon.xpguardian;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class XPGuardian extends JavaPlugin implements Listener {
@@ -11,6 +16,20 @@ public class XPGuardian extends JavaPlugin implements Listener {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info(this + " enabled.");
+    }
+    
+    @EventHandler(priority = EventPriority.LOW)
+    public void onPlayerXPDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        Entity killer = player.getKiller();
+        
+        if (killer instanceof Player && !player.hasPermission("xpguardian.pvp")) {
+            return;
+        } else if (!(killer instanceof Player) && !player.hasPermission("xpguardian.pve")) {
+            return;
+        }
+        event.setKeepLevel(true);
+        event.setDroppedExp(0);
     }
 }
 
